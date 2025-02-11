60d0a9d91c9a1e307d6f37b39cbf3c947b8b970d
jairoreina <jfreina1@gmail.com>
2025-02-11 07:54:08 -0500

    open links in new tab + change about card height on mobile

--- frontend/src/components/AboutCard.tsx
+++ frontend/src/components/AboutCard.tsx
@@ -22,7 +22,7 @@ export function AboutCard({ isOpen, onClose }: AboutCardProps) {
   
   return (
     <div className="fixed inset-0 bg-black/50 flex items-center justify-center p-4 z-50" onClick={onClose}>
-      <div className="bg-white shadow-lg max-w-2xl w-full p-6 relative max-h-[90vh] overflow-y-auto rounded-xl" onClick={(e) => e.stopPropagation()}>
+      <div className="bg-white shadow-lg max-w-2xl w-full p-6 my-8 relative max-h-[80vh] sm:max-h-[90vh] overflow-y-auto rounded-xl" onClick={(e) => e.stopPropagation()}>
         <button
           onClick={onClose}
           className="absolute top-4 right-4 text-gray-500 hover:text-gray-700"
--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -4,16 +4,16 @@ export function Footer() {
             <div className="flex flex-col items-center justify-center">
             <div className="flex items-center justify-center">
                 <p className="text-sm text-gray-500 text-center mb-2">
-                Made by <a href="https://jairoreina.com" className="underline">Jairo Reina</a>
+                Made by <a href="https://jairoreina.com" target="_blank" rel="noopener noreferrer" className="underline">Jairo Reina</a>
                 </p>
             </div>
             <div className="flex items-center justify-center">
                 <p className="text-sm text-gray-500">Find me on:</p>
                 <p className="text-sm text-gray-500 mx-2">
-                <a href="https://github.com/jairoreina" className="underline">GitHub</a>
+                <a href="https://github.com/jairoreina" target="_blank" rel="noopener noreferrer" className="underline">GitHub</a>
                 </p>
                 <p className="text-sm text-gray-500 mx-2">
-                <a href="https://www.x.com/jreina9" className="underline">X/Twitter</a>
+                <a href="https://www.x.com/jreina9" target="_blank" rel="noopener noreferrer" className="underline">X/Twitter</a>
                 </p>
             </div>
             </div>
